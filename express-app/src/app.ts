573eb6a024b513f810cf599f90a3a9cd93c289b1
Muhammad_Sharaf <muhammad.sharafeldin@nabtahealth.com>
2024-08-03 15:00:23 +0300

    initial docker setup

--- express-app/src/app.ts
+++ express-app/src/app.ts
@@ -1,8 +1,18 @@
 import express, {Express} from 'express';
 import {json} from 'body-parser';
+import {Sequelize} from "sequelize";
+
+import sequelize from './utils/database';
 
 const app: Express = express();
 
 app.use(json());
 
-app.listen(3000);
+sequelize.sync().then((res: Sequelize) => {
+    console.log('Database synchronized');
+    app.listen(3000, () => {
+        console.log('Server listening on port 3000');
+    });
+}).catch((err: Sequelize) => {
+    console.log('err', err);
+})
